065ec1974dc158c4e2805af0436e4024f73b0efa
otherPOM <otherPOM@gmail.com>
2020-12-16 18:00:06 +0300

    Stage 2 done

--- Cinema.java
+++ Cinema.java
@@ -1,16 +1,34 @@
 package cinema;
 
+import java.util.Scanner;
+
 public class Cinema {
+    private int rows;
+    private int seats;
+
+    public Cinema(int rows, int seats) {
+        this.rows = rows;
+        this.seats = seats;
+    }
+
+    public int calcIncome() {
+        var seatsTotal = rows * seats;
+        if (seatsTotal <= 60) {
+            return seatsTotal * 10;
+        } else {
+            var frontRows = rows / 2;
+            var backRows = rows - frontRows;
+            return (frontRows * seats * 10) + (backRows * seats * 8);
+        }
+    }
 
     public static void main(String[] args) {
-        System.out.println("Cinema:\n" +
-                "  1 2 3 4 5 6 7 8\n" +
-                "1 S S S S S S S S\n" +
-                "2 S S S S S S S S\n" +
-                "3 S S S S S S S S\n" +
-                "4 S S S S S S S S\n" +
-                "5 S S S S S S S S\n" +
-                "6 S S S S S S S S\n" +
-                "7 S S S S S S S S");
+        var scan = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        var rows = scan.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        var seats = scan.nextInt();
+        var cinema = new Cinema(rows, seats);
+        System.out.printf("Total income:\n$%d", cinema.calcIncome());
     }
 }

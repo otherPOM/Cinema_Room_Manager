bba1d2ea26f6298ac0ae450331dab20aa79e58e0
otherPOM <otherPOM@gmail.com>
2020-12-16 18:57:41 +0300

    Stage 3 done

--- Cinema.java
+++ Cinema.java
@@ -1,25 +1,56 @@
 package cinema;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Cinema {
     private int rows;
     private int seats;
+    private boolean big;
+    private char[][] visual;
 
     public Cinema(int rows, int seats) {
         this.rows = rows;
         this.seats = seats;
+        big = rows * seats > 60;
+        visual = new char[rows][seats];
+        for (char[] row : visual) {
+            Arrays.fill(row, 'S');
+        }
+    }
+
+    public String getVisual() {
+        var sb = new StringBuilder(" ");
+        for (int i = 1; i <= seats; i++) {
+            sb.append(" ").append(i);
+        }
+        sb.append('\n');
+        for (int i = 0; i < rows; i++) {
+            sb.append(i + 1);
+            for (char c : visual[i]) {
+                sb.append(' ').append(c);
+            }
+            sb.append('\n');
+        }
+        return sb.toString();
     }
 
     public int calcIncome() {
-        var seatsTotal = rows * seats;
-        if (seatsTotal <= 60) {
-            return seatsTotal * 10;
+        if (big) {
+            return rows * seats * 10;
         } else {
             var frontRows = rows / 2;
             var backRows = rows - frontRows;
             return (frontRows * seats * 10) + (backRows * seats * 8);
         }
+    }
+
+    public int getSeatPrice(int rowN) {
+        return !big ? 10 : rowN <= rows / 2 ? 10 : 8;
+    }
+
+    public void bought(int rowN, int seatN) {
+        visual[rowN - 1][seatN - 1] = 'B';
     }
 
     public static void main(String[] args) {
@@ -28,7 +59,19 @@ public class Cinema {
         var rows = scan.nextInt();
         System.out.println("Enter the number of seats in each row:");
         var seats = scan.nextInt();
+
         var cinema = new Cinema(rows, seats);
-        System.out.printf("Total income:\n$%d", cinema.calcIncome());
+        System.out.println("Cinema:");
+        System.out.println(cinema.getVisual());
+        System.out.println("Enter a row number:");
+        var rowN = scan.nextInt();
+        System.out.println("Enter a seat number in that row:");
+        var seatN = scan.nextInt();
+        System.out.printf("Ticket price: $%d\n", cinema.getSeatPrice(rowN));
+        cinema.bought(rowN, seatN);
+        System.out.println("Cinema:");
+        System.out.println(cinema.getVisual());
+
+//        System.out.printf("Total income:\n$%d", cinema.calcIncome());
     }
 }
--- README.md
+++ README.md
@@ -26,3 +26,10 @@ In this stage, you need to read two positive integer numbers from the input: the
 
 Calculate the profit from the sold tickets depending on the number of seats and print the result as shown in the examples below. After that, your program should stop. Note that in this project, the number of rows and seats won't be greater than 9.
 
+## Stage 3
+Read two positive integer numbers that represent the number of rows and seats in each row and print the seating arrangement like in the first stage. Then, read two integer numbers from the input: a row number and a seat number in that row. These numbers represent the coordinates of the seat according to which the program should print the ticket price. The ticket price is determined by the same rules as the previous stage:
+
+* If the total number of seats in the screen room is not more than 60, then the price of each ticket is 10 dollars.
+* In a larger room, the tickets are 10 dollars for the front half of the rows and 8 dollars for the back half. Please note that the number of rows can be odd, for example, 9 rows. In this case, the first half is the first 4 rows, and the second half is the rest 5 rows.
+
+After that, the program should print out all the seats in the screen room as shown in the example and mark the chosen seat by the B symbol. Finally, it should print the ticket price and stop. Note that in this project, the number of rows and seats won't be greater than 9.
